de77bbcda26efa5ee8835baccac688849100ef25
Lucas Henrique <lucashfsdev@gmail.com>
2024-12-25 23:11:11 -0300

    feat: Configured the default server port and the default route file.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Eu existo");
+const express = require("express");
+const routers = require("./src/routers/dog");
+const app = new express();
+const PORT = 3000;
+app.use(express.json());
+app.use(routers);
+
+app.listen(PORT, () => {
+  console.log(`Server started : http://localhost:${PORT}`);
+});
